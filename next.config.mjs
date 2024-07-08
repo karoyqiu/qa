ab5fc918e1aa8fd6cc3dcc127d38930f12327fa4
Roy QIU <karoyqiu@gmail.com>
2024-07-08 21:43:28 +0800

    尝试添加严格的 CSP。

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,45 @@
+const DEV = process.env.NODE_ENV !== 'production';
+
+const cspHeader = `
+  default-src 'self';
+  base-uri 'self';
+  script-src 'self' ${DEV ? `'unsafe-inline' 'unsafe-eval'` : ''};
+  object-src 'none';
+  frame-ancestors 'none';
+  ${DEV ? '' : 'upgrade-insecure-requests;'}
+`
+  .replace(/\s{2,}/g, ' ')
+  .trim();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   crossOrigin: 'anonymous',
   output: 'standalone',
   poweredByHeader: false,
+  compiler: {
+    reactRemoveProperties: !DEV,
+    removeConsole: !DEV && {
+      exclude: ['error'],
+    },
+  },
   experimental: {
     optimizePackageImports: ['radash'],
   },
+  headers: () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Content-Security-Policy',
+          value: cspHeader,
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
