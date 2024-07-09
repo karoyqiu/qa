b110f7cd55951c33bebd0e0937b69a05eecae229
Roy QIU <karoyqiu@gmail.com>
2024-07-09 14:03:53 +0800

    再加点头。

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,78 @@ const cspHeader = `
   .replace(/\s{2,}/g, ' ')
   .trim();
 
+const ppDirectives = [
+  'accelerometer',
+  'ambient-light-sensor',
+  'attribution-reporting',
+  'autoplay',
+  'battery',
+  'bluetooth',
+  'browsing-topics',
+  'camera',
+  'compute-pressure',
+  'display-capture',
+  'domain-agent',
+  'document-domain',
+  'encrypted-media',
+  'execution-while-not-rendered',
+  'execution-while-out-of-viewport',
+  'fullscreen',
+  'gamepad',
+  'geolocation',
+  'gyroscope',
+  'hid',
+  'identity-credentials-get',
+  'idle-detection',
+  'local-fonts',
+  'magnetometer',
+  'microphone',
+  'midi',
+  'otp-credentials',
+  'payment',
+  'picture-in-picture',
+  'publickey-credentials-create',
+  'publickey-credentials-get',
+  'screen-wake-lock',
+  'serial',
+  'speaker-selection',
+  'storage-access',
+  'usb',
+  'web-share',
+  'window-management',
+  'xr-spatial-tracking',
+];
+
+const ppHeader = ppDirectives.map((d) => `${d}=()`).join();
+
+const extraHeaders = [
+  {
+    source: '/(.*)',
+    headers: [
+      {
+        key: 'Content-Security-Policy',
+        value: cspHeader,
+      },
+      {
+        key: 'Permissions-Policy',
+        value: ppHeader,
+      },
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+      },
+      {
+        key: 'X-Frame-Options',
+        value: 'DENY',
+      },
+      {
+        key: 'X-XSS-Protection',
+        value: '1;mode=block',
+      },
+    ],
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   crossOrigin: 'anonymous',
@@ -28,21 +100,7 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['radash'],
   },
-  headers: () => [
-    {
-      source: '/(.*)',
-      headers: [
-        {
-          key: 'Content-Security-Policy',
-          value: cspHeader,
-        },
-        {
-          key: 'X-Content-Type-Options',
-          value: 'nosniff',
-        },
-      ],
-    },
-  ],
+  headers: () => extraHeaders,
 };
 
 export default nextConfig;
